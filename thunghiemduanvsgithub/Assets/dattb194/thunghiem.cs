30d05625d674edf896dcadb7d095cd7e13a524bc
dattb94 <dattb94@gmail.com>
2018-07-03 14:26:53 +0700

    Revert "change1"
    This reverts commit 337052f3232a601fe5a55f8ca94ceb33d9d3db9c.

--- thunghiemduanvsgithub/Assets/dattb194/thunghiem.cs
+++ thunghiemduanvsgithub/Assets/dattb194/thunghiem.cs
@@ -1,29 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class thunghiem : MonoBehaviour {
 	GameObject a;
 
-<<<<<<< HEAD
-//change1
-//chnage2
-=======
-    ///dattb1994_change
-	// Use this for initialization
 
-    /// comit1
-    //comit2
-    //comit3
-<<<<<<< HEAD
-<<<<<<< HEAD
-    //comit4
-    //comit5
-=======
->>>>>>> parent of 73bf7d2... 3
-=======
->>>>>>> parent of 73bf7d2... 3
->>>>>>> parent of 337052f... change1
 	void Start () {
 		
 	}

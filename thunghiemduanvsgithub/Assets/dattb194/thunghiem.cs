337052f3232a601fe5a55f8ca94ceb33d9d3db9c
dattb94 <dattb94@gmail.com>
2018-07-03 14:23:00 +0700

    change1
    change1

--- thunghiemduanvsgithub/Assets/dattb194/thunghiem.cs
+++ thunghiemduanvsgithub/Assets/dattb194/thunghiem.cs
@@ -1,24 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class thunghiem : MonoBehaviour {
 	GameObject a;
 
-    ///dattb1994_change
-	// Use this for initialization
+//change1
 
-    /// comit1
-    //comit2
-    //comit3
-<<<<<<< HEAD
-<<<<<<< HEAD
-    //comit4
-    //comit5
-=======
->>>>>>> parent of 73bf7d2... 3
-=======
->>>>>>> parent of 73bf7d2... 3
 	void Start () {
 		
 	}
